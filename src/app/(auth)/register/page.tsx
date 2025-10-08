9b2181a4771651afe82a86dcc168f5c451541232
Khayrul Islam <khayrulislam2525@gmail.com>
2025-10-08 21:08:58 +0600

    login register form add

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,8 @@
+import RegistrationForm from '@/components/auth/RegistrationForm';
+import React from 'react';
+
+const Register = () => {
+  return <RegistrationForm />;
+};
+
+export default Register;
---
+++ src/components/auth/RegistrationForm.tsx
@@ -0,0 +1,95 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { ChangeEvent, useState } from 'react';
+
+export default function RegistrationForm() {
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  console.log(formData);
+
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>Create new account</CardTitle>
+        <CardDescription>Enter details to create your account</CardDescription>
+        <CardAction>
+          <Button variant="link">Sign In</Button>
+        </CardAction>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="name">Name</Label>
+              <Input
+                id="name"
+                type="name"
+                placeholder="Jhon Doe"
+                name="name"
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                name="email"
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <a
+                  href="#"
+                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+              <Input
+                id="password"
+                type="password"
+                name="password"
+                onChange={handleChange}
+                required
+              />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <Button type="submit" className="w-full">
+          Register
+        </Button>
+        <Button variant="outline" className="w-full">
+          Register with Google
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
